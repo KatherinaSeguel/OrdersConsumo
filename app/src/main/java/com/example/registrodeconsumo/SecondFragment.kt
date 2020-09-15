004e98ce3d7d4a0f31d46f6f8092898e80b8c234
katthy <you@example.com>
2020-09-14 21:06:42 -0300

    Entity creada Dao Creado Database Creada Repositorio empezando el ViewModel ViewModel terminando itemlist arreglado adapter recyclerView instancia el adaptador y mostrar datos guardado se moeve botoncito del segundo frgmaento al primer fragmento segundo fragmento ya graba en el RecyclerView Edición de datos falta borrar los datos Bundle del primer al segundo fragento editando datos en el segundo fragmento

--- app/src/main/java/com/example/registrodeconsumo/SecondFragment.kt
+++ app/src/main/java/com/example/registrodeconsumo/SecondFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.example.registrodeconsumo.database.Pedidos
@@ -40,6 +41,11 @@ class SecondFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        idpedidos?.let {
+            mViewModel.getOnePedidosByID(it).observe(viewLifecycleOwner, Observer {
+
+            })
+        }
 
 
 
